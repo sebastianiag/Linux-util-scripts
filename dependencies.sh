9078bbde16ccd39f58d7ade79fc19cd9da332113
sebastianiag <suushimaan@gmail.com>
2015-09-04 12:18:21 -0400

    first script for i3 compilation

---
+++ dependencies.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-xinerama0-dev
